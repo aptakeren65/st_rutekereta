2b70b36dcbd339d88c8ce4a30f35b4bb667be50e
aptakeren65 <aptakeren65@gmail.com>
2026-05-26 13:51:32 +0700

    kereta_app.py

--- kereta_app.py
+++ kereta_app.py
@@ -106,7 +106,7 @@ with st.sidebar:
 
 # ==================== PILIHAN MENU 1: CARI RUTE ====================
 if pilihan_menu == "📍 Cari Rute Terpendek":
-    st.title("🚇 Pencari Rute Terpendek Kereta")
+    st.title("🚇 Pencari Rute Terpendek Kereta Air")
     st.write("Temukan jalur kereta api tercepat menggunakan efisiensi algoritma.")
     st.markdown("---")
 
